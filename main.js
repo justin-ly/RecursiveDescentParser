3da924b9ea68ccd7694ba56cd39b6860b621e2a3
Justin Ly <justinsongly@gmail.com>
2017-04-27 17:59:35 -0700

    Finished TERM(). Recursive Descent Parser complete! (for now)

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 /*
     VALID TESTS:
-    1. 3*3$
-    2. (3*3)$
-    3. (3*3*3/3)$
-    4. (3*3)/(3*3)$
-    5.
+    1. 3*3$         //Multiply
+    2. (3*3)$       // (EXP)
+    3. (3*3*3/3)$   // (EXP)
+    4. (3*3)/(3*3)$ // (EXP) * (EXP)
+    5. 
     6.
     7.
 
     INVALID TESTS:
-    1. (((3*3$
-    2. (3*3)/3*3)$
+    1. (((3*3$      //Mismatched parenthesis (Extra left parenthesis)
+    2. (3*3)/3*3)$  //Mismatched parenthesis (Extra right parenthesis)
+    3. (1-4)*3$     //4 is not allowed
+    4. 22-3$        //Two-digit numbers not allowed
 
     Ex: http://athena.ecs.csus.edu/~rdp135/rdr/rdr1.php
 */
 
 let input;
-let result;
+let result = true;
 let pos = 0;
 
 function start() {
-    pos = 0;
+    //Re-initialize all variables every time a new string is entered by the user.
     input = document.getElementById("input").value;
-    console.log(input);
+    pos = 0;
+    result = true;
 
+    //Hide the result message. It is valid until an error is found in the string.
     document.getElementById("result").style.visibility = "hidden";
     document.getElementById("result").innerHTML = "The input " + input + " is valid.";
 
@@ -33,15 +37,25 @@ function start() {
     if (input.charAt(pos) != '$') {
         invalid();
     }
-    document.getElementById("result").style.visibility = "initial";
+
+    document.getElementById("result").style.visibility = "initial"; //Show result message
+
+    if (result) {
+        document.getElementById("result").style.color = "limegreen";
+    }
+    else {
+        document.getElementById("result").style.color = "red";
+    }
 }
 
 
-// EXP ::= TERM {(+|-)TERM}
 function EXP() {
     TERM();
+    if (input.charAt(pos) == '+' || input.charAt(pos) == '-') {
+        pos++;
+        EXP();
+    }
 }
-
 
 function TERM() {
     FACTOR();
@@ -52,7 +66,6 @@ function TERM() {
 }
 
 
-// FACTOR ::= (EXP) | DIGIT
 function FACTOR() {
     if (input.charAt(pos) == '(') {
         pos++;
@@ -64,19 +77,19 @@ function FACTOR() {
             invalid();
         }
     }
-    else { 
+    else {
         DIGIT();
     }
 }
 
 
 function DIGIT() {
-    //console.log('Correctly called digit: ', input.charAt(pos));
     if (input.charAt(pos) in ['0', '1', '2', '3']) pos++;
     else invalid();
 }
 
 function invalid() {
-    console.log('Invalid because of ', input.charAt(pos));
+    result = false;
     document.getElementById("result").innerHTML = "The input " + input + " is invalid.";
 }
+
